69f1db2e9cfa4f09502506efb5b2d5b255615e6f
Trey Brister <webstandardcss@gmail.com>
2015-07-04 19:59:31 -0500

    contact

---
+++ contact.php
@@ -0,0 +1,285 @@
+<?php include_once('includes/header.php'); ?>
+
+<section id="page-banner">
+	<!-- IF Banner src is not blank and "No banner is not selected" -->
+			
+			<div class="bg-cover bg-cc" style="background-image:url('/assets/images/headerpattern.png');">
+
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 pd-v-lg banner-col" style="display: table;">
+							<div style="display: table-cell;vertical-align: middle; text-align: center">
+								<h1 class="headfont heavyfont upperfont tx-white text-center" style="lvertical-align: middle;">Voting Rights</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+</section>
+
+<div class="wrapper wrapperMain noLogos clearfix pRel">
+
+<div id="signupheader"></div>
+
+<!--Main Title: One large line in an "h1" tag-->
+<header class="mainHeader"><h1>Contact Us</h1>
+</header><div class="gridC brgrayL">
+	<div class="row">
+
+<article class="bgwhite five column module"><div class="entry-content">
+<!--Body Copy: each paragraph in "p" tags-->
+<p>Please submit all political and public policy—related questions and comments, as well as any technical issues or questions about Democrats.org here.</p>
+
+<p>You can also reach us via mail or phone.</p>
+
+<p><strong>Mailing address:</strong><br>
+Democratic National Committee<br>
+430 South Capitol St. SE<br>
+Washington, DC 20003</p>
+
+<p><strong>Main phone number:</strong> <span class="tel value">202-863-8000</span></p>
+
+<p>For questions about contributions, please call <span class="tel value">877-336-7200.</span></p>
+
+<p>If you want to contact us regarding <a href="http://www.democrats.org/internships">internships</a> or <a href="http://www.democrats.org/about/work-with-us">jobs</a>, please see the appropriate pages.</p>
+
+<!--highlighted secondary call to action: one line in "p" and "strong" tags-->
+
+</div>
+
+</article><div class="five column omega">
+
+<!--Call to Action: Very short line in "h3" tag-->
+<h4 class="Museo caps">We want to hear from you</h4>
+
+
+<div></div><br><form name="signup" class="bsd-signup-969" action="/page/s/contact-the-democrats" method="post" id="signup" style="visibility: visible;"><ol class="onePerLine full"><li class="li-firstname li-text" id="li-firstname"><label class="field" for="firstname">First Name<span class="required">*</span></label><input size="16" id="firstname" name="firstname" type="text" title="heuristic type: NAME_FIRST
+    server type: NAME_FIRST
+    field signature: 3763331450
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></li><li class="li-lastname li-text" id="li-lastname"><label class="field" for="lastname">Last Name<span class="required">*</span></label><input size="25" id="lastname" name="lastname" type="text" title="heuristic type: NAME_LAST
+    server type: NAME_LAST
+    field signature: 3494530716
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></li><li class="li-email" id="li-email"><label class="field" for="email">Email<span class="required">*</span></label><input type="email" class="text" size="48" id="email" name="email" title="heuristic type: EMAIL_ADDRESS
+    server type: EMAIL_ADDRESS
+    field signature: 1029417091
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></li><li class="li-addr1 li-text" id="li-addr1"><label class="field" for="addr1">Address</label><input size="48" id="addr1" name="addr1" type="text" title="heuristic type: ADDRESS_HOME_LINE1
+    server type: NO_SERVER_DATA
+    field signature: 2734389730
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></li><li class="li-state_cd li-select" id="li-state_cd"><label class="field" for="state_cd">State</label><select id="state_cd" name="state_cd" title="heuristic type: ADDRESS_HOME_STATE
+    server type: ADDRESS_HOME_STATE
+    field signature: 3511756169
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"><option value=""></option><option value="AK">AK</option><option value="AL">AL</option><option value="AR">AR</option><option value="AZ">AZ</option><option value="CA">CA</option><option value="CO">CO</option><option value="CT">CT</option><option value="DC">DC</option><option value="DE">DE</option><option value="FL">FL</option><option value="GA">GA</option><option value="HI">HI</option><option value="IA">IA</option><option value="ID">ID</option><option value="IL">IL</option><option value="IN">IN</option><option value="KS">KS</option><option value="KY">KY</option><option value="LA">LA</option><option value="MA">MA</option><option value="MD">MD</option><option value="ME">ME</option><option value="MI">MI</option><option value="MN">MN</option><option value="MO">MO</option><option value="MS">MS</option><option value="MT">MT</option><option value="NC">NC</option><option value="ND">ND</option><option value="NE">NE</option><option value="NH">NH</option><option value="NJ">NJ</option><option value="NM">NM</option><option value="NV">NV</option><option value="NY">NY</option><option value="OH">OH</option><option value="OK">OK</option><option value="OR">OR</option><option value="PA">PA</option><option value="RI">RI</option><option value="SC">SC</option><option value="SD">SD</option><option value="TN">TN</option><option value="TX">TX</option><option value="UT">UT</option><option value="VA">VA</option><option value="VT">VT</option><option value="WA">WA</option><option value="WI">WI</option><option value="WV">WV</option><option value="WY">WY</option><option disabled="disabled" value="---">--</option><option value="AA">AA</option><option value="AE">AE</option><option value="AP">AP</option><option value="AS">AS</option><option value="FM">FM</option><option value="GU">GU</option><option value="MH">MH</option><option value="MP">MP</option><option value="PR">PR</option><option value="PW">PW</option><option value="VI">VI</option></select></li><li class="li-zip li-text" id="li-zip"><label class="field" for="zip">Zip<span class="required">*</span></label><input size="8" id="zip" name="zip" type="text" title="heuristic type: ADDRESS_HOME_ZIP
+    server type: ADDRESS_HOME_ZIP
+    field signature: 569224803
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></li><li class="li-phone li-tel" id="li-phone"><label class="field" for="phone">Phone</label><input type="tel" class="text" size="15" id="phone" name="phone" title="heuristic type: PHONE_HOME_WHOLE_NUMBER
+    server type: PHONE_HOME_WHOLE_NUMBER
+    field signature: 1794903826
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></li><li class="li-custom li-textarea" id="li-custom-21396"><label class="field" for="custom-21396">Your Comments or Questions<span class="required">*</span></label><textarea rows="5" cols="50" id="custom-21396" name="custom-21396" title="heuristic type: UNKNOWN_TYPE
+    server type: NO_SERVER_DATA
+    field signature: 3955409914
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></textarea></li><li class="li-submit-btn li-submit" id="li-submit-btn"><label class="field"></label><input name="submit-btn" value="Submit" type="submit" class="button Prox white bgblueD"></li></ol>
+<div id="required_note"><span class="required">*</span><span style="font-size:80%;"> denotes required field</span></div><table id="signuptable" style="display: none;"><tbody><tr><td>
+<table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-email">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-firstname">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-lastname">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr><td valign="top">
+        <div class="fieldset" id="bsd-field-addr1">
+            <div class="label"></div>
+            <div class="input"></div></div>
+        </td></tr><tr><td><input size="48" id="addr2" name="addr2" type="text" title="heuristic type: UNKNOWN_TYPE
+    server type: ADDRESS_HOME_LINE2
+    field signature: 153243481
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-state_cd">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-zip">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-phone">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-custom-21396">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><div style="display: none"><label for="best-contact-time">Please leave this field blank:</label> <input id="best-contact-time" name="best-contact-time" type="text" title="heuristic type: UNKNOWN_TYPE
+    server type: NO_SERVER_DATA
+    field signature: 1437388407
+    form signature: 2720558594203660506
+    experiment id: &quot;&quot;"></div><table width="100%"><tbody><tr><td valign="top">
+    <div class="fieldset" id="bsd-field-submit-btn">
+        <div class="label"></div>
+        <div class="input"></div></div>
+    </td></tr></tbody></table></td></tr><tr><td><table width="100%"><tbody><tr></tr><input name="redirect_url" type="hidden" value=""><input id="_guid" name="_guid" type="hidden" value=""></tbody></table></td>
+</tr></tbody></table></form><div id="signupfooter"></div>
+<script>
+var ops = { 
+submitText: "Submit",  //will be the text in the button, defaults to BSD text
+submitColor: "bgblueD", //should be one of the following: bgredL, bgblueD, or another defined brand color. defaults to light red
+formType: "onePerLine full", // layout mode for form: defaults to onePerLine: twoEven will do conventional first,last,email zip
+emailmove: true, //if true, will move the email field below firstname/lastname
+zipmove: false //if true, will move the zip field to after the email field
+};
+
+</script></div>
+<script type="text/javascript">
+    bQuery.bsd.spud('populateForm', 'signup',
+    [
+        'email',
+        'firstname',
+        'lastname',
+        'addr1',
+        'addr2',
+        'city',
+        'state_cd',
+        'zip',
+        'country',
+        'phone'
+    ]);
+
+
+    var submitted_state_cd = null;
+
+    var spudSent = false;
+    bQuery(document).ready(function() {
+        bQuery('#signup').submit(function(e) {
+
+            var guid = bQuery.cookie('guid');
+            if (typeof guid == 'string' && guid.length == 23) {
+                bQuery('#_guid').val(guid);
+            }
+
+            if (!spudSent) {
+                bQuery.bsd.spud('setFromForm', 'signup',
+                [
+                    'email',
+                    'firstname',
+                    'lastname',
+                    'addr1',
+                    'addr2',
+                    'city',
+                    'state_cd',
+                    'zip',
+                    'country',
+                    'phone'
+                 ], function () {
+                     spudSent = true;
+                     setTimeout(function() {bQuery('#signup').trigger('submit');}, 300);
+                 });
+                 e.preventDefault();
+             }
+
+        });
+    });
+</script><script type="text/javascript">
+        bQuery('#email').blur(
+            function() {
+                bQuery(this).mailcheck({
+                                                domains: ["aol.com","yahoo.com","hotmail.com","gmail.com","msn.com","comcast.net","juno.com","earthlink.net","sbcglobal.net","verizon.net","bellsouth.net","cs.com","cox.net","worldnet.att.net","att.net","charter.net","webtv.net","mac.com","netscape.net","excite.com","mindspring.com","prodigy.net","optonline.net","adelphia.net","netzero.net"],
+                                                        suggested: function(element, suggestion) {
+                               bQuery(element).parents('div.fieldset').children('.bsd-mailcheck').remove();
+
+                               bQuery(element).parents('div.fieldset').append(
+                                   '<span class="error bsd-mailcheck">Did you mean ' +
+                                   '<a class="bsd-mailcheck-fix-link" href="#">' + 
+                                   suggestion.domain + '<' + '/a>?<' + '/span>');
+
+                               bQuery(element).parents('div.fieldset').find(".bsd-mailcheck-fix-link").click(
+                                   function() {
+                                       bQuery(element).val(suggestion.address + '@' + suggestion.domain);
+                                       bQuery(element).parents('div.fieldset').children('.bsd-mailcheck').remove();
+                                   });
+                           },
+                               empty: function(element) {
+                               bQuery(element).parents('div.fieldset').children('.bsd-mailcheck').remove();
+                           }
+                    });
+            });
+</script><div id="fb-root" class=" fb_reset"><div style="position: absolute; top: -10000px; height: 0px; width: 0px;"><div><iframe name="fb_xdm_frame_http" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" id="fb_xdm_frame_http" aria-hidden="true" title="Facebook Cross Domain Communication Frame" tabindex="-1" src="http://static.ak.facebook.com/connect/xd_arbiter/xRlIuTsSMoE.js?version=41#channel=f11ad10f5&amp;origin=http%3A%2F%2Fmy.democrats.org" style="border: none;"></iframe><iframe name="fb_xdm_frame_https" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" id="fb_xdm_frame_https" aria-hidden="true" title="Facebook Cross Domain Communication Frame" tabindex="-1" src="https://s-static.ak.facebook.com/connect/xd_arbiter/xRlIuTsSMoE.js?version=41#channel=f11ad10f5&amp;origin=http%3A%2F%2Fmy.democrats.org" style="border: none;"></iframe></div></div><div style="position: absolute; top: -10000px; height: 0px; width: 0px;"><div></div></div></div>		</div>
+	</div>
+</div>
+
+<section>
+	<div class="sidebar-extender"></div>
+	<div class="container">
+		<div class="row">
+			<!-- IF sidebar exists -->
+			<div class="row-same-height">
+			    <div class="col-md-8 col-sm-7 col-xs-height col-top bg-white using-sidebar-left">
+					<!-- IF header is not blank -->
+					<div class="row">
+						<div class="col-md-12" style="padding-bottom: 45px;">
+							<div class="channel-page-body content-block">
+								<div class="pd-v-sm heavyfont upperfont">&nbsp;</div>
+																
+								<p>Democrats have a long and proud history of fighting for voting rights that continues to this day. And while we've made significant progress in securing the right to vote for all eligible Americans, many voters still face difficulties in the voting process, from registering to casting a ballot to having their votes counted.</p>
+
+								<p>Those often disproportionately affected are communities of color, young people, the elderly, low-income individuals, and disabled voters, as well as military members and veterans. In many parts of the country, voters are underserved by a lack of polling places, outdated voting machines, and unnecessarily complicated laws.</p>
+
+								<p>As Republican politicians try to make it harder to vote, Democrats are working to expand access to the polls. And we won't stop working to promote a system of elections that is accessible, open, and fair — a system that ensures that every eligible person can cast a vote and that every lawfully cast vote is counted.</p>
+
+								<p>To learn more about why voting rights matter, visit&nbsp;<a href="http://www.votingrightsmatter.com">www.votingrightsmatter.com</a>.</p>
+							</div>
+							<div class="pd-v-md"></div>
+						</div>
+						<div class="col-md-12">
+
+						</div>
+					</div>
+				</div>
+			
+		    	<div class="col-md-4 col-sm-5 col-xs-height col-top hidden-xs using-sidebar-right full-h" style="min-height: 200px; padding-right: 0px;">
+		    		<div class="pd-h-md bg-gray full-h">
+			    		<div class="pd-v-sm heavyfont upperfont">&nbsp;</div>
+					   	<div class="heavyfont headfont upperfont tx-navy font-md">Resources</div>
+					    <div class="pd-v-vs sidebar-link">
+							<a class="tx-navy" href="http://s3.amazonaws.com/uploads.democrats.org/Downloads/Dems-report-real_cost_of_voting_ID.pdf">
+								<div style="display: table-cell;vertical-align: top;" class="sidebar-number">
+					    			<span class="btn headfont heavyfont bd-black btn-sm" style="min-width:50px; display:block;">1</span>
+					    		</div>
+						    	<div style="display: table-cell;padding-left:10px;vertical-align: middle;" class="sidebar-label">
+					    			 <span class="font-md bodyfont">The Real Cost of Voter ID</span>
+					    		</div>
+					    	</a>
+					    </div>
+						<div class="pd-v-vs sidebar-link">
+							<a class="tx-navy" href="//www.democrats.org/voting-rights-institute">
+								<div style="display: table-cell;vertical-align: top;" class="sidebar-number">
+					    			<span class="" style="min-width:50px; display:block;">2</span>
+					    		</div>
+						    	<div style="display: table-cell;padding-left:10px;vertical-align: middle;" class="sidebar-label">
+					    			 <span class="font-md bodyfont">Voting Rights Institute</span>
+					    		</div>
+					    	</a>
+					    </div>
+				    	<div class="pd-md clearfix"></div>
+				    </div>
+		    	</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php include_once('includes/footer.php'); ?>
