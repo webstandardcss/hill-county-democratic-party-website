53d7b5d72dc2ec3913c1f55721ce18d921f467f8
Trey Brister <webstandardcss@gmail.com>
2015-07-04 19:42:51 -0500

    format html

--- blank.php
+++ blank.php
@@ -22,58 +22,58 @@
 	<div class="container">
 		<div class="row">
 			<!-- IF sidebar exists -->
-				<div class="row-same-height">
-				    <div class="col-md-8 col-sm-7 col-xs-height col-top bg-white using-sidebar-left">
-						<!-- IF header is not blank -->
-						<div class="row">
-							<div class="col-md-12" style="padding-bottom: 45px;">
-								<div class="channel-page-body content-block">
-									<div class="pd-v-sm heavyfont upperfont">&nbsp;</div>
-																	
-									<p>Democrats have a long and proud history of fighting for voting rights that continues to this day. And while we've made significant progress in securing the right to vote for all eligible Americans, many voters still face difficulties in the voting process, from registering to casting a ballot to having their votes counted.</p>
+			<div class="row-same-height">
+			    <div class="col-md-8 col-sm-7 col-xs-height col-top bg-white using-sidebar-left">
+					<!-- IF header is not blank -->
+					<div class="row">
+						<div class="col-md-12" style="padding-bottom: 45px;">
+							<div class="channel-page-body content-block">
+								<div class="pd-v-sm heavyfont upperfont">&nbsp;</div>
+																
+								<p>Democrats have a long and proud history of fighting for voting rights that continues to this day. And while we've made significant progress in securing the right to vote for all eligible Americans, many voters still face difficulties in the voting process, from registering to casting a ballot to having their votes counted.</p>
 
-									<p>Those often disproportionately affected are communities of color, young people, the elderly, low-income individuals, and disabled voters, as well as military members and veterans. In many parts of the country, voters are underserved by a lack of polling places, outdated voting machines, and unnecessarily complicated laws.</p>
+								<p>Those often disproportionately affected are communities of color, young people, the elderly, low-income individuals, and disabled voters, as well as military members and veterans. In many parts of the country, voters are underserved by a lack of polling places, outdated voting machines, and unnecessarily complicated laws.</p>
 
-									<p>As Republican politicians try to make it harder to vote, Democrats are working to expand access to the polls. And we won't stop working to promote a system of elections that is accessible, open, and fair — a system that ensures that every eligible person can cast a vote and that every lawfully cast vote is counted.</p>
+								<p>As Republican politicians try to make it harder to vote, Democrats are working to expand access to the polls. And we won't stop working to promote a system of elections that is accessible, open, and fair — a system that ensures that every eligible person can cast a vote and that every lawfully cast vote is counted.</p>
 
-									<p>To learn more about why voting rights matter, visit&nbsp;<a href="http://www.votingrightsmatter.com">www.votingrightsmatter.com</a>.</p>
-								</div>
-								<div class="pd-v-md"></div>
+								<p>To learn more about why voting rights matter, visit&nbsp;<a href="http://www.votingrightsmatter.com">www.votingrightsmatter.com</a>.</p>
 							</div>
-							<div class="col-md-12">
+							<div class="pd-v-md"></div>
+						</div>
+						<div class="col-md-12">
 
-							</div>
 						</div>
 					</div>
-				
-			    	<div class="col-md-4 col-sm-5 col-xs-height col-top hidden-xs using-sidebar-right full-h" style="min-height: 200px; padding-right: 0px;">
-			    		<div class="pd-h-md bg-gray full-h">
-				    		<div class="pd-v-sm heavyfont upperfont">&nbsp;</div>
-						    	<div class="heavyfont headfont upperfont tx-navy font-md">Resources</div>
-							    <div class="pd-v-vs sidebar-link">
-									<a class="tx-navy" href="http://s3.amazonaws.com/uploads.democrats.org/Downloads/Dems-report-real_cost_of_voting_ID.pdf">
-										<div style="display: table-cell;vertical-align: top;" class="sidebar-number">
-							    			<span class="btn headfont heavyfont bd-black btn-sm" style="min-width:50px; display:block;">1</span>
-							    		</div>
-								    	<div style="display: table-cell;padding-left:10px;vertical-align: middle;" class="sidebar-label">
-							    			 <span class="font-md bodyfont">The Real Cost of Voter ID</span>
-							    		</div>
-							    	</a>
-							    </div>
-								<div class="pd-v-vs sidebar-link">
-									<a class="tx-navy" href="//www.democrats.org/voting-rights-institute">
-										<div style="display: table-cell;vertical-align: top;" class="sidebar-number">
-							    			<span class="" style="min-width:50px; display:block;">2</span>
-							    		</div>
-								    	<div style="display: table-cell;padding-left:10px;vertical-align: middle;" class="sidebar-label">
-							    			 <span class="font-md bodyfont">Voting Rights Institute</span>
-							    		</div>
-							    	</a>
-							    </div>
-					    	<div class="pd-md clearfix"></div>
-					    </div>
-			    	</div>
 				</div>
+			
+		    	<div class="col-md-4 col-sm-5 col-xs-height col-top hidden-xs using-sidebar-right full-h" style="min-height: 200px; padding-right: 0px;">
+		    		<div class="pd-h-md bg-gray full-h">
+			    		<div class="pd-v-sm heavyfont upperfont">&nbsp;</div>
+					   	<div class="heavyfont headfont upperfont tx-navy font-md">Resources</div>
+					    <div class="pd-v-vs sidebar-link">
+							<a class="tx-navy" href="http://s3.amazonaws.com/uploads.democrats.org/Downloads/Dems-report-real_cost_of_voting_ID.pdf">
+								<div style="display: table-cell;vertical-align: top;" class="sidebar-number">
+					    			<span class="btn headfont heavyfont bd-black btn-sm" style="min-width:50px; display:block;">1</span>
+					    		</div>
+						    	<div style="display: table-cell;padding-left:10px;vertical-align: middle;" class="sidebar-label">
+					    			 <span class="font-md bodyfont">The Real Cost of Voter ID</span>
+					    		</div>
+					    	</a>
+					    </div>
+						<div class="pd-v-vs sidebar-link">
+							<a class="tx-navy" href="//www.democrats.org/voting-rights-institute">
+								<div style="display: table-cell;vertical-align: top;" class="sidebar-number">
+					    			<span class="" style="min-width:50px; display:block;">2</span>
+					    		</div>
+						    	<div style="display: table-cell;padding-left:10px;vertical-align: middle;" class="sidebar-label">
+					    			 <span class="font-md bodyfont">Voting Rights Institute</span>
+					    		</div>
+					    	</a>
+					    </div>
+				    	<div class="pd-md clearfix"></div>
+				    </div>
+		    	</div>
+			</div>
 		</div>
 	</div>
 </section>
