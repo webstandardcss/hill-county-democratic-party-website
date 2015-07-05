e51c6a34a90997aa4f5f41931f896cb756ff818e
Trey Brister <webstandardcss@gmail.com>
2015-07-04 20:09:50 -0500

    one-column.php

---
+++ one-column.php
@@ -0,0 +1,59 @@
+<?php include_once('includes/header.php'); ?>
+
+<section id="page-banner">
+<!-- IF Banner src is not blank and "No banner is not selected" -->
+	<div class="bg-cover bg-cc" style="background-image:url('//d3rk1vod59vegc.cloudfront.net/http_s3.amazonaws.com/uploads.democrats.org/Banners/headerpattern.png');">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 pd-v-lg banner-col" style="display: table;">
+					<div style="display: table-cell;vertical-align: middle; text-align: center">
+						<h1 class="headfont heavyfont upperfont tx-white text-center" style="lvertical-align: middle;">Retirement Security</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<!-- IF sidebar exists -->
+			<div class="row-same-height">
+				<div class="col-md-12 col-xs-height col-top bg-white">
+					<!-- IF header is not blank -->
+					<div class="row">
+						<div class="col-md-12" style="padding-bottom: 45px;">
+							<div class="channel-page-body content-block">
+								<div class="pd-v-sm heavyfont upperfont">&nbsp;</div>
+								<p>Democrats believe that a dignified retirement is central to the American Dream, and its foundation is built on two long-standing institutions charged with realizing that dream: Medicare and Social Security. These two institutions represent an unbreakable commitment to American workers, and for decades Democrats have fought to defend them.</p>
+
+								<p>In 1935, Democrats and President Franklin Roosevelt created Social Security. In 1965, Democrats and President Lyndon Johnson created Medicare. Ever since, Democrats have continually fought to defend these cornerstones of the American Dream in the face of attempts to dismantle or undermine both.</p>
+
+								<h5>PROTECTING SOCIAL SECURITY</h5>
+
+								<p>For 75 years, Social Security has been an enduring promise to America’s seniors. Today it remains a safety net for seniors and offers all Americans peace of mind. In recent years, Democrats have beaten back Republican plans to privatize Social Security — plans that would have exposed the retirement funds of millions of American seniors to great risk on the eve of the financial crisis. Instead, no one lost a penny of Social Security.</p>
+
+								<h5>STRENGTHENING MEDICARE</h5>
+
+								<p>Medicare is the second pillar of Democrats’ commitment to Americans’ retirement security. Health care&nbsp;reform strengthened the Medicare trust fund, expanding its life by more than a decade. The Affordable Care Act has also&nbsp;improved care across the board, reduced fraud, and finally closed the hole in Medicare drug coverage known as the “donut hole.” Seniors will continue to have full access to their doctors and are now&nbsp;eligible for free coverage for certain preventive services, including an annual wellness exam.</p>
+
+								<h5>HELPING AMERICANS SAVE FOR RETIREMENT</h5>
+
+								<p>Democrats are committed to making it easier for Americans to save for retirement on their own. Nearly half the workforce — about 75 million people — currently do not have employer-supported retirement plans. Democrats want to make it easier for all Americans to participate in retirement accounts at work and support a system where employees have pension portability, so workers don’t lose their pension if they change jobs.</p>
+
+								<p>Democrats believe that all Americans have the right to a secure and healthy retirement, and we will continue fighting to preserve both Medicare and Social Security for future generations.</p>
+							</div>
+							<div class="pd-v-md"></div>
+						</div>
+						<div class="col-md-12">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('includes/footer.php'); ?>
